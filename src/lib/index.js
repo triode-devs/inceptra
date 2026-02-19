d6a8a49ceccc7601f5c8916120d736ec64b7baed
triode-devs <triodedevs@gmail.com>
2026-02-19 17:13:51 +0530

    Update API_BASE_URL to production endpoint

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://192.168.137.224:8787';
+//export const API_BASE_URL = 'http://192.168.137.224:8787';
 
-//export const API_BASE_URL = 'https://inceptra-api.triodedevs.workers.dev';
+export const API_BASE_URL = 'https://inceptra-api.triodedevs.workers.dev';
