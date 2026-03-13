b0a65dd4812d50e17d685303637ba5c60eb81402
triode-devs <triodedevs@gmail.com>
2026-03-13 10:52:58 +0530

    Fix formatting issue in hackathon guidelines

--- src/lib/data/data.js
+++ src/lib/data/data.js
@@ -466,7 +466,7 @@ export const rawHackathonRules = {
         'Internet access is allowed.',
         'Pre-built libraries and APIs are allowed.',
         'Pre-developed code is NOT allowed (except open-source components).',
-        'Teams must develop the core idea during hackathon hours.'
+        'Teams must develop the core idea during hackathon hours.',
         'All team members are required to register individually through the registration form.'
     ],
     submission_requirements: [
